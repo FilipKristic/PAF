3dae6e1d8de06bd2ec2e27bb767908bb28901fcd
FilipKristic <fkristic@pmfst.hr>
2026-03-04 11:20:41 +0100

    Moj prvi commit 123

---
+++ Vj8.1.py
@@ -0,0 +1,8 @@
+student={"ime":"Josip", "prezime":"Josipović", "godine":32}
+print(student["godine"])
+print(student["prezime"])
+print(student["ime"])
+for key in student:
+    print(str(student[key])+" Jeej")
+
+
